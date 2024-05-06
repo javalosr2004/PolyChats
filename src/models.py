8b444e7fc814f04e1c7e3067009c6d2a2a36933f
noahgiboney <noahgiboney@gmail.com>
2024-05-06 08:14:49 -0700

    model tables?

--- src/models.py
+++ src/models.py
@@ -2,5 +2,14 @@ import sqlalchemy
 from src import database
 
 # SQL table for Users
-meta_user = sqlalchemy.MetaData()
-user_table = sqlalchemy.Table("User", meta_user, autoload_with=database.engine)
+metaData = sqlalchemy.MetaData()
+user_table = sqlalchemy.Table("User", metaData, autoload_with=database.engine)
+
+# SQL table for Comment
+comment_table = sqlalchemy.Table("Comment", metaData, autoload_with=database.engine)
+
+# SQL table for Posts
+post_table = sqlalchemy.Table("Posts", metaData, autoload_with=database.engine)
+
+# SQL table for Followers
+message_table = sqlalchemy.Table("Followers", metaData, autoload_with=database.engine)
