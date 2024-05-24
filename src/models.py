fa3db7d97783778a222459ae12307d8d670a262c
Jesus Avalos <jesusavalosr2004@gmail.com>
2024-05-23 17:17:22 -0700

    updates to models

--- src/models.py
+++ src/models.py
@@ -3,20 +3,30 @@ from src import database
 
 # SQL table for Users
 user_table_metadata = sqlalchemy.MetaData()
-user_table = sqlalchemy.Table("User", user_table_metadata, autoload_with=database.engine)
+user_table = sqlalchemy.Table(
+    "User", user_table_metadata, autoload_with=database.engine)
 
 # SQL table for Comment
 comment_table_metadata = sqlalchemy.MetaData()
-comment_table = sqlalchemy.Table("Comments", comment_table_metadata , autoload_with=database.engine)
+comment_table = sqlalchemy.Table(
+    "Comments", comment_table_metadata, autoload_with=database.engine)
 
 # SQL table for Posts
 posts_table_metadata = sqlalchemy.MetaData()
-post_table = sqlalchemy.Table("Posts", posts_table_metadata, autoload_with=database.engine)
+post_table = sqlalchemy.Table(
+    "Posts", posts_table_metadata, autoload_with=database.engine)
 
 # SQL table for Followers
 followers_table_metadata = sqlalchemy.MetaData()
-followers_table = sqlalchemy.Table("Followers", followers_table_metadata, autoload_with=database.engine)
+followers_table = sqlalchemy.Table(
+    "Followers", followers_table_metadata, autoload_with=database.engine)
 
 # SQL table for Likes/Dislikes (Reactions)
 reactions_table_metadata = sqlalchemy.MetaData()
-reations_table = sqlalchemy.Table("Reactions", reactions_table_metadata, autoload_with=database.engine)
+reations_table = sqlalchemy.Table(
+    "Reactions", reactions_table_metadata, autoload_with=database.engine)
+
+# SQL table for Profile
+profile_table_metadata = sqlalchemy.MetaData()
+profile_table = sqlalchemy.Table(
+    "Profile", profile_table_metadata, autoload_with=database.engine)
